1aab62788afba2ae705d427613566afaed651b6a
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-04-20 18:35:16 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ * 
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+
 int main(void);
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
