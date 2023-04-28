f92f739d0bba907fb44b03a9961a84f2b46c3bb1
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-04-27 19:31:20 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - hecks for uppercase character
+ *
+ * @c: is the checked number
+ *
+ * Return: 1 uppercase , 0 lowercase
+*/
+
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' || c <= 'Z'i)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+int _putchar (char ch);
+void print_alphabet(void);
+int _isupper(int c);
+
+
+
+
+#endif
