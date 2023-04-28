28e5cbf971a4da3bd91b51057272c3af7c2fe92f
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-04-27 21:52:16 -0700

    my commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,12 @@
  * mul - Entry point
  * a function that multiplies two integers
  *
+ * @a, b: the operated integers
+ *
  * Return: result
 */
 
 int mul(int a, int b)
 {
-	int a, b;
-
 	return (a * b);
 }
