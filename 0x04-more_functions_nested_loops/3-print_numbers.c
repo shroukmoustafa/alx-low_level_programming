2ca8f52c3af4c46a2336a670ffd3c7ba9617b842
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-04-27 22:17:01 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * a function that prints the numbers, from 0 to 9
+ * followed by a new line
+ *
+ * Return: 0 (success)
+*/
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar('i');
+	}
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ extern void print_alphabet(void);
 extern int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
+extern void print_numbers(void);
 
 
 #endif
