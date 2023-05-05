1e8cd75b5579ddf08add8a9b3f3bc3c94e04993f
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-04 19:02:34 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+int _atoi(char *s);
+void rev_string(char *s);
+char *_strcat(char *dest, char *src);
+
+#endif
