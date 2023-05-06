86b6ad6b5e982b337b501666a94d065908633bbf
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-05 19:52:21 -0700

    my commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  * Return: (str)
 */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0', i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
