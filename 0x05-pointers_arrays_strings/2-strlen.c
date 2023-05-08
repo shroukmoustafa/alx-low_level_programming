7c02ff8045ceb945f8484e19c903e4414f3aa2af
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-07 20:22:20 -0700

    my commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,14 @@
 #include "main.h"
- /**
+/**
   * _strlen - a function that returns the length of a string.
-  * 
-  * @s: the string to be measured */
+  *
+  * @s: the string to be measured
+*/
 
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (*s >= 32)
 	{
 		i++;
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,13 +14,14 @@ void print_rev(char *s)
 	i = 0;
 	while (*s != '\0')
 	{
-		s++;
 		i++;
+		s++;
 	}
-	while (i >= 0)
+	while (i > 0)
 	{
 		_putchar(*s);
 		s--;
+		i--;
 	}
-
+	_putchar("\n");
 }
