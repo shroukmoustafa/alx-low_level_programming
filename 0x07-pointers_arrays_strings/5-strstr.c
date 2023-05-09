af8807f7a833a2a3cf7bbde034554b9ff0b97632
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-08 20:00:06 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr -  a function that locates a substring.
+ *
+ * @haystack: the compared string
+ * @needle: the string to be found
+ *
+ * Return: (haystack)
+*/
+
+char *_strstr(char *haystack, char *needle)
