8a648d86683b9d6e893c7414d5d82788220588b2
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-11 18:46:20 -0700

    my commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _prime_number - a function that returns 1 if the
+ * is_prime_number - a function that returns 1 if the
  *  input integer is a prime number, otherwise return 0.
  *
  *  @n: the number to be checked
@@ -12,7 +12,27 @@
 int is_prime_number(int n)
 {
 	if (n == 1)
-	       return (0);
-	if (n /= _prime_number(	
+		return (0);
+	if (n <= 0)
+		return (0);
+	return (dividing(n, (n - 1)));
+}
 
+/**
+ * dividing -  a function that checks the mouduls of n
+ *
+ * @n: the number to be checked
+ * @x:is a natural number less than n
+ *
+ * Return: (1) if the number is prime, (1) if the number not prime
+*/
+
+int dividing(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	if (n % x != 0)
+		dividing(n, (x - 1));
+	else if (n % x == 0)
+		return (0)
 }
