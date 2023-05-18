ec9ff3d3b07cb989411347566fd3875f01c3cb60
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-17 19:13:15 -0700

    my commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,8 +16,10 @@ int main(int argc, char **argv)
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
+		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
+		}
 			i += atoi(argv[argc]);
 	}
 	printf("%d\n", i);
