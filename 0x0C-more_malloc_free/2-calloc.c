b714e8139927885e678c13d9cb6ca898155bc179
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-05-19 18:24:38 -0700

    my commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,7 +28,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(int) * nmemb);
+	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
 	_memset(p, 0, sizeof(int) * nmemb);
