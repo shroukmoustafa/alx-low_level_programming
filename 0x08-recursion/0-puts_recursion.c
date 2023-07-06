0c396e27c4441dd9284ee8a597eaaec96ed612df
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-05 18:20:37 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == "\0")
+	if (*s == '\0')
 	{
-		_putchar ("\n");
+		_putchar ('\n');
+		return;
 	}
-	_putchar (*s);
-	_puts_recursion(s++);
+	_putchar ((char) *s);
+	_puts_recursion(s + 1);
 }
