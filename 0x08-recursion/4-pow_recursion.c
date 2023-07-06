cedef029111a11ab7fb26fcdc9e800adab0cde8e
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-06 15:20:33 -0700

    power function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
- /**
-  * _pow_recursion - a function that returns the value
+/**
+  *_pow_recursion - a function that returns the value
   *  of x raised to the power of y
   *  @x: the number raised to thr power
   *  @y: the value of the power
-  *  Return : (1) if y equal to zero, otherwise (result)
- */
+  *Return: (1) if y equal to zero, otherwise (result)
+*/
 int _pow_recursion(int x, int y)
 {
 	int sqrt;
