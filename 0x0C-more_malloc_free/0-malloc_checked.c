d38cf9e0c546967be65f78495e48d8cf65fb36c0
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-13 15:39:39 -0700

    my commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: number of bites
+ * Return: pointer on sucess , 98 on failuer
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr,
+	
+	ptr = malloc (b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
