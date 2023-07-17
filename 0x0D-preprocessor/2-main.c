f7d8f7cb847d5411ef34f70c44c1ffba298b4465
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-17 13:54:51 -0700

    macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #define PRINT_FILENAME printf("%s\n", __FILE__)
 
+/**
+ *main - a program that prints the name of the file it was compiled from,
+ followed by a new line
+ *Return: (0) on success
+*/
 int main(void)
 {
 	PRINT_FILENAME;
