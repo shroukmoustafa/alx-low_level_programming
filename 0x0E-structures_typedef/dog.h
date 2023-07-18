d0e903f947226cbbb4876a4c2ec7fb922415b636
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-18 16:48:58 -0700

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,12 @@
  * @owner: owners name
  * Description: a dog information structure
 */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-	};
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
