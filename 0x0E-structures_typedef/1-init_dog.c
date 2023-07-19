85179fcfa53944a3171b70489895421c0fe44c27
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-18 17:48:59 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,10 +9,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
+	if (d != NULL)
+{
 	d ->name = name;
 	d->age = age;
 	d->owner = owner;
+}
 }
