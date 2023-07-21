7d2cb39b6bc1a13464bc1e139ffbb75cae0a39d4
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-20 19:00:42 -0700

    mt commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
+/**
+ * struct token - struct token
+ * @token: format token
+ * @f: the function associated
+*/
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
