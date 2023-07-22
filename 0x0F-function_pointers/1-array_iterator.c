fd686c0fdb3965a388c02a497f5c6e31167c5326
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-21 18:09:46 -0700

    my commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 	{
-	for (i = 0; 1 < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
