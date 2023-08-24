e7e9697cfc3ecde6939c6abb95d0982c73ef9bab
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-24 05:39:07 -0700

    my commit' my commiy

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask = 1UL << index;
 
 	if (index >= (sizeof(unsigned long int) * 8))
-		return -1;
+		return (-1);
 	if (n & mask)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index.
+ * @n: pointer
+ * @index: index
+ * Return: 1 on sucess, 0 on failuer
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	unsigned long int mask = 1UL << index;
+	*n |= mask;
+	return (1);
+}
