6af85b93119dfeb797985db681192afcd13b1997
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-24 10:25:15 -0700

    my commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * binary_to_uint - a function that converts a binary number 
+ * binary_to_uint - a function that converts a binary number
  * to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars
  * Return: the unsigned int number
