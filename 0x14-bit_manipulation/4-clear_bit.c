b43c3b2f71c4ae6b7e4c7412b3afcd987e4e10dd
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-24 05:55:59 -0700

    my commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - a function that sets the value of a bit to 0 at a given index.
+ * @n: the adressed number
+ * @index: index
+ * Return: 1 if it worked, or -1 if an error occurred
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	return (-1);
+	mask = 1UL << index;
+	mask = ~mask;
+	*n &= mask;
+	return (1);
+}
