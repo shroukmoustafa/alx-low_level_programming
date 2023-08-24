5eaecae52528e431add2d6fff24ba48e927cb4da
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-24 05:41:32 -0700

    my commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,8 +7,6 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
-
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	unsigned long int mask = 1UL << index;
