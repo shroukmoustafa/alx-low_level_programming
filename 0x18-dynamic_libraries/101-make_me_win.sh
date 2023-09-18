9293b02ccbfa6cfaf592d53f229f37ec5ff66f93
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-09-18 12:26:58 -0700

    m

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p https://github.com/shroukmoustafa/alx-low_level_programming/raw/blob/master/0x18-dynamic_libraries/libgiga.so
+wget -p /tmp https://github.com/shroukmoustafa/alx-low_level_programming/raw/blob/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
