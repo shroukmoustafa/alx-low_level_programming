d792908a19f8af8eb3ebd906cfa9d452cdd12efd
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-09-22 05:18:28 -0700

    s

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_list: a function that prints all the elements of a list_t list.
+ * print_list - a function that prints all the elements of a list_t list.
  * @h: head pointer
  * Return: the number of nodes
 */
