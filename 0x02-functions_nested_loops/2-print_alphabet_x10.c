6caef05e8c40aa3b0506612c21e882b984c507f8
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-03-04 03:52:41 -0800

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alpabet - program prints the alphabet
+ * followed by new line
+ *
+ * return: always 0 (success)
+*/
+
+void print_alphabet(void)
+{       char ch;
+	for (i = 1; i < 11; i++)
+	{
+        	for (ch = 'a'; ch <= 'z'; ch++)
+        	{
+                	_putchar (ch);
+        	}
+		i++;
+	}
+        _putchar('\n');
+}
+
